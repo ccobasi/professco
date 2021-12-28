cd879924538caa8278110cc107d803a6b91373b6
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-12-28 21:55:04 +0100

    initial commit

---
+++ courses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.conf import settings
+from django.contrib.auth import get_user_model
+import uuid
+# from mutagen.mp4 import MP4,MP4StreamInfoError
+# from .helpers import get_timer
+# from decimal import Decimal
+
+# from cloudinary_storage.storage import MediaCloudinaryStorage
+# from cloudinary_storage.validators import validate_video
+
+# from cloudinary.models import CloudinaryField
